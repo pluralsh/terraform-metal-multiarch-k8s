fdc2801970f18388d232f80e60b502d4cc16f583
Marques Johansson <mjohansson@equinix.com>
2021-12-09 17:32:07 -0500

    replace docker with containerd
    Signed-off-by: Marques Johansson <mjohansson@equinix.com>

--- main.tf
+++ main.tf
@@ -5,4 +5,11 @@ provider "metal" {
 resource "metal_project" "new_project" {
   count = var.metal_create_project ? 1 : 0
   name  = var.metal_project_name
+
+  # Kube-vip will enable BGP if not enabled, Terraform must match the settings
+  bgp_config {
+    deployment_type = "local"
+    md5             = ""
+    asn             = 65000
+  }
 }
